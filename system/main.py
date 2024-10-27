07697c5c7687ca2724377e3ffa92c485b6d1a6c3
Senura Hansaja <ifire810@gmail.com>
2024-10-27 16:43:53 +0000

    cifar10

--- system/main.py
+++ system/main.py
@@ -402,7 +402,7 @@ if __name__ == "__main__":
     parser.add_argument('-dev', "--device", type=str, default="cuda",
                         choices=["cpu", "cuda"])
     parser.add_argument('-did', "--device_id", type=str, default="0")
-    parser.add_argument('-data', "--dataset", type=str, default="MNIST")
+    parser.add_argument('-data', "--dataset", type=str, default="Cifar10")
     parser.add_argument('-nb', "--num_classes", type=int, default=10)
     parser.add_argument('-m', "--model", type=str, default="CNN")
     parser.add_argument('-lbs', "--batch_size", type=int, default=10)
